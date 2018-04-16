3cb2ac64600fc69c30c0882d0be28783ab8e0043
jkwu <jkwu@amarsoft.com>
2018-04-16 23:23:36 +0800

    feat:  add test example program in test directory

--- test/sort-children-object.js
+++ test/sort-children-object.js
@@ -1,26 +1,3 @@
-var object = {
-  10: 'a',
-  age: '20',
-  20: {
-    nuber1: 'jkuw',
-    class: 'class4',
-    number2: 'jack',
-    7: 'number',
-    2: 'haha',
-    acount: '400'
-  },
-  LN:'里宁',
-  7: 'c',
-  sex: 'male',
-  name: 'jkwu',
-  group: {
-    name: '永恒',
-    column: 1,
-    member: '50',
-    7: 'number',
-    2: 'haha'
-  }
-};
 // var object = {
 //   10: 'a',
 //   age: '20',
@@ -30,35 +7,7 @@ var object = {
 //     number2: 'jack',
 //     7: 'number',
 //     2: 'haha',
-//     acount: {
-//       nuber1: 'jkuw',
-//       class: 'class4',
-//       number2: 'jack',
-//       7: 'number',
-//       2: 'haha',
-//       acount: {
-//         nuber1: 'jkuw',
-//         class: 'class4',
-//         number2: 'jack',
-//         7: 'number',
-//         2: 'haha',
-//         acount: {
-//           nuber1: 'jkuw',
-//           class: 'class4',
-//           number2: 'jack',
-//           7: 'number',
-//           2: 'haha',
-//           acount: {
-//             nuber1: 'jkuw',
-//             class: 'class4',
-//             number2: 'jack',
-//             7: 'number',
-//             2: 'haha',
-//             acount: '400'
-//           }
-//         }
-//       }
-//     }
+//     acount: '400'
 //   },
 //   LN:'里宁',
 //   7: 'c',
@@ -69,9 +18,75 @@ var object = {
 //     column: 1,
 //     member: '50',
 //     7: 'number',
-//     2: 'haha',
+//     2: 'haha'
 //   }
 // };
+var object = {
+  10: 'a',
+  age: '20',
+  20: {
+    nuber1: 'jkuw',
+    class: 'class4',
+    number2: 'jack',
+    7: 'number',
+    2: 'haha',
+    acount: {
+      nuber1: 'jkuw',
+      class: 'class4',
+      number2: 'jack',
+      7: 'number',
+      2: 'haha',
+      acount: {
+        nuber1: 'jkuw',
+        class: 'class4',
+        number2: 'jack',
+        7: 'number',
+        2: 'haha',
+        acount: {
+          nuber1: 'jkuw',
+          class: 'class4',
+          number2: 'jack',
+          7: 'number',
+          2: 'haha',
+          acount: {
+            nuber1: 'jkuw',
+            class: 'class4',
+            number2: 'jack',
+            7: 'number',
+            2: 'haha',
+            acount: '400'
+          }
+        }
+      }
+    }
+  },
+  LN:'里宁',
+  7: 'c',
+  sex: 'male',
+  name: 'jkwu',
+  group: {
+    name: '永恒',
+    column: 1,
+    member: '50',
+    7: 'number',
+    2: 'haha',
+  }
+};
+
 var sortObjectKeys = require('../index');
+var fs = require('fs');
 var sortedObject = sortObjectKeys(object);
+
+// 同步保存json文件
+function saveFileSync(jsonObj, filePath) {
+  try {
+    const data = JSON.stringify(jsonObj, null, 2);
+    fs.writeFileSync(filePath, data);
+  } catch (err) {
+    log('文件保存失败:' + err);
+    throw err;
+  }
+}
+
+saveFileSync(sortedObject, 'sortedObjectResult.json');
 console.log(sortedObject);
